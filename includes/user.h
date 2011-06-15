58d60d5cbef68ce36a81e4456c1f8eb668549a37
Misha Manulis <misha@manulis.com>
2011-06-15 00:15:03 -0700

    Some initial work, does not compile

--- includes/user.h
+++ includes/user.h
@@ -6,13 +6,15 @@
 #include "forrst_list.h"
 #include "post.h"
 
-#ifndef ERROR
-#define ERROR -1
+#ifndef FAIL
+#define FAIL 0
 #endif
 
 #ifndef SUCCESS
-#define SUCCESS
+#define SUCCESS 1
 #endif
+
+#define FORRST_API_USER_INFO_URL "http://forrst.com/api/v2/users/info"
 
 enum forrst_user_type {
 	DEVELOPER,
@@ -21,28 +23,29 @@ enum forrst_user_type {
 } ;
 
 struct forrst_user {
-	uint64_t				id ;
+	char*				    id ;
+  size_t          idLen ;
 	char*						username ;
-	uint32_t				usernameLen ;
+	size_t				  usernameLen ;
 	char*						name ;
-	uint32_t				nameLen ;
+	size_t				  nameLen ;
 	char*						url ;
-	uint32_t				urlLen ;
-	uint32_t				numOfPosts ;
-	uint32_t				numOfComments ;
-	uint32_t				numOfLikes ;
-	uint32_t				numOfFollowers ;
-	uint32_t				numOfFollowing ;
+	size_t				  urlLen ;
+	size_t				  numOfPosts ;
+	size_t				  numOfComments ;
+	size_t				  numOfLikes ;
+	size_t				  numOfFollowers ;
+	size_t				  numOfFollowing ;
 	char*						bio ;
-	uint64_t				bioLen ;
+	size_t          bioLen ;
 	enum forrst_user_type	isA ;
 	char*						homepage ;
-	uint32_t				homepageLen ;
+	size_t          homepageLen ;
 	char*					  twitter ;
-	uint32_t				twitterLen ;
+	size_t          twitterLen ;
 	int							inDirectory ;
 	char*						tags ;
-	uint32_t				tagsLen ;
+	size_t          tagsLen ;
 	//
 	// :TODO: Photos?
 	//
@@ -50,18 +53,18 @@ struct forrst_user {
 
 int
 forrst_user_auth( char* username, uint32_t usernameLen,
-					 char* password, uint32_t passwordLen,
-					 char** authToken, uint32_t authTokenLen ) ;
+					        char* password, uint32_t passwordLen,
+					        char** authToken, uint32_t authTokenLen ) ;
 
 int
 forrst_user_info( uint32_t userId, char* username, uint32_t usernameLen,
-		       struct forrst_user userInfo ) ;
+		              struct forrst_user* userInfo ) ;
 
 int
 forrst_user_posts( uint32_t userId, char* username, uint32_t usernameLen,
-					  enum forrst_post_type postType, uint32_t postLimit,
-						uint32_t afterPostId,
-						struct forrst_list** usersPosts ) ;
+					         enum forrst_post_type postType, uint32_t postLimit,
+						       uint32_t afterPostId,
+						       struct forrst_list** usersPosts ) ;
 
 #endif
 
--- src/user.c
+++ src/user.c
@@ -1,4 +1,44 @@
 #include "user.h"
 
+#include "connection.h"
 
+int
+forrst_user_info( char* userId, size_t userIdLen,
+                  char* username, size_t usernameLen,
+		              struct forrst_user* userInfo ) {
+  char* full_url = NULL ;
+  char* user_id = NULL ;
+  //
+  if( NULL == username || 0 == usernameLen ||
+      strlen(username) != usernameLen || NULL == userInfo ) {
+    return FAIL ;
+  }
+  //
+  user_id = (char*)malloc( sizeof(char) * (
+                           )) ;
+  full_url = (char*)malloc( sizeof(char) * (
+                              strlen(FORRST_API_USER_INFO_URL) +
+                              usernameLen + strlen(itoa(userId))
+                            )) ;
+  //
+  return FAIL ;
+}
+
+int
+forrst_user_auth( char* username, uint32_t usernameLen,
+					        char* password, uint32_t passwordLen,
+					        char** authToken, uint32_t authTokenLen ) {
+  // :NOT WORKING:
+  //
+  return FAIL ;
+}
+
+int
+forrst_user_posts( uint32_t userId, char* username, uint32_t usernameLen,
+					         enum forrst_post_type postType, uint32_t postLimit,
+						       uint32_t afterPostId,
+						       struct forrst_list** usersPosts ) {
+  //
+  return FAIL ;
+}
 
